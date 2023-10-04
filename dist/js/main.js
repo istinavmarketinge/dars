b24b73f4b638f5da5be8cba00f5d564ea6bdc645
Olya Krugova <j-olga.k@yandex.ru>
2023-10-04 15:47:51 +0300

    сделала окно Спасибо за вашу заявку

--- dist/js/main.js
+++ dist/js/main.js
@@ -95,6 +95,12 @@ if (document.querySelector(".affiliate-program__more ")) {
     $('.affiliate-program__hide-details').on('click', function () {
       $('.affiliate-program__right').removeClass('open');
     });
+    $('.affiliate-program__button').on('click', function () {
+      $('.thanks-you').addClass('open');
+    });
+    $('.thanks-you__close').on('click', function () {
+      $('.thanks-you').removeClass('open');
+    });
   });
 }
 
